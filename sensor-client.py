0d54c747375ed07cae7a304c7f78db308ed889fe
hungnguyen2510 <htmm2510@gmail.com>
2020-11-14 16:19:43 +0700

    fix

--- sensor-client.py
+++ sensor-client.py
@@ -15,7 +15,4 @@ async def ClientSend():
         await websocket.send(s)
         print(f"> {s}")
 
-        res = await websocket.recv()
-        print(f"< {res}")
-
 asyncio.get_event_loop().run_until_complete(ClientSend())
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(data) {
+    console.log("Client sent: ", data);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
